af1cfd6d8d74cb85af4b4ef0e0c5a198f875b938
Rafael Jeffman <rafael.jeffman@gmail.com>
2014-03-18 18:54:33 -0300

    Merged with old repository.

--- ListasEncadeadas/src/com/senac/estruturas/Nodo.java
+++ ListasEncadeadas/src/com/senac/estruturas/Nodo.java
@@ -1,7 +1,6 @@
 package com.senac.estruturas;
 
 public class Nodo<T> {
-<<<<<<< HEAD
 	private T chave;
 	private Nodo<T> next;
 	
@@ -26,36 +25,4 @@ public class Nodo<T> {
 	public void setNext(Nodo<T> next) {
 		this.next = next;
 	}
-=======
-	
-	private T  dado;
-	private Nodo<T> next;
-
-	public Nodo(T i)
-	{
-		dado = i;
-		next = null;
-	}
-	
-	public void setData(T i)
-	{
-		dado = i;
-		next = null;
-	}
-	
-	public T getData()
-	{
-		return dado;
-	}
-
-	public void setNext(Nodo<T> next)
-	{
-		this.next = next;
-	}
-	
-	public Nodo<T> getNext()
-	{
-		return next;
-	}
->>>>>>> d6310f1e61ad2aae30ae94e4bff23a5990af1507
 }
